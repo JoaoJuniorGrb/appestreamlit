7945b76ea58a775527a8dbd3575b3b8fd3ab83bb
JoaoJuniorGrb <JoaoJuniorGrb>
2024-02-06 20:48:48 -0300

    primeiro commit

---
+++ yahoo_finance.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import streamlit as st
+
+st.title("Preço do ativo")
+st.header("Informações a respeito do fechamento de ações")
+
+opcoes = st.selectbox('Escolha o ativo',('BBAS3.SA','BBDC4.SA','PETR4.SA','VALE5.SA'))
+
+tickerSimbolo = opcoes
+tickerData = yf.Ticker(tickerSimbolo)
+
+tickerDF = tickerData.history(period = "1d",start="2012-5-19",end="2022-5-19")
+print(tickerDF)
+st.header("Grafico de Fechamento")
+st.line_chart(tickerDF.Close)
+
+st.header("Grafico de volume")
+st.line_chart(tickerDF.Volume)
+
+st.header("Grafico de dividendos")
+st.line_chart(tickerDF.Dividends)
+
